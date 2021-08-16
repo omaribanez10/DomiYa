a067a8797922103a963fdef0b9342744d14a619d
Omar Ibanez <omaribanez100@gmail.com>
2021-08-15 20:02:18 -0500

    Creando las vistas e instalando librerías.

---
+++ app/TopNegocios.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TopNegocios() {
+  return (
+    <View>
+      <Text>Top Negocios...</Text>
+    </View>
+  );
+}
