6b0e97a12d2e7a1064f84f1cb4bd19c752e3082a
Omar Ibanez <omaribanez100@gmail.com>
2021-12-30 12:29:55 -0500

    Vista Login

--- app/navigations/CuentaStack.js
+++ app/navigations/CuentaStack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Cuenta from "../screens/cuenta/Cuenta";
+import Login from "../screens/cuenta/Login";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,11 @@ export default function CuentaStack() {
         name="cuenta"
         component={Cuenta}
         options={{ title: "Cuenta" }}
+      ></Stack.Screen>
+       <Stack.Screen
+        name="login"
+        component={Login}
+        options={{ title: "Iniciar Sesión" }}
       ></Stack.Screen>
     </Stack.Navigator>
   );
