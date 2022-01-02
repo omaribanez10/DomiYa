f50435bbe072e739ca2f03baee1eba007cf79916
Omar Ibanez <omaribanez100@gmail.com>
2022-01-01 22:00:21 -0500

    Terminado el formulario de registro

--- app/components/Cuenta/FormularioRegistro.js
+++ app/components/Cuenta/FormularioRegistro.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
+import Loading from "../Loading";
 import { validateEmail } from "../../utils/validations";
 import { size, isEmpty } from "lodash";
 import firebase from "firebase/app";
 import {useNavigation} from "@react-navigation/native"
+
 
 export default function FormularioRegistro(props) {
   const { toastRef } = props;
   const [showPassword, setShowPassword] = useState(false);
   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
   const [formData, setformData] = useState(defaultFormValue());
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
 
   const onSubmit = () => {
@@ -29,13 +32,17 @@ export default function FormularioRegistro(props) {
         "Las contraseñas tienen que ser mayor a 6 caracteres"
       );
     } else {
+      setLoading(true);
       firebase
         .auth()
         .createUserWithEmailAndPassword(formData.email, formData.password)
         .then((response) => {
+          setLoading(false);
           navigation.navigate("cuenta");
+          
         })
         .catch(() => {
+          setLoading(false);
           toastRef.current.show("El correo electrónico ya se encuentra registrado, intente con otro.");
         });
     }
@@ -97,6 +104,7 @@ export default function FormularioRegistro(props) {
         buttonStyle={styles.btnRegister}
         onPress={onSubmit}
       />
+      <Loading isVisible={loading} text="Registrando usuario."/>
     </View>
   );
 }
